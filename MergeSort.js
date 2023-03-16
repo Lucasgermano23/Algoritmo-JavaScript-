97b8ce7b8bfeb775bd3a4db56c4cceb634fbc0e8
Lucasgermano23 <45275693+Lucasgermano23@users.noreply.github.com>
2023-03-15 23:09:08 -0300

    Update MergeSort.js

--- MergeSort.js
+++ MergeSort.js
@@ -1,37 +1,42 @@
 const listaLivros = require('./');
 
 function mergeSort(array){
+  
   if(array.length > 1){
   cosnt meio = Math.floor(array.length / 2); 
   const parte1 = mergeSort(array.slice(0, meio));
   cosnt parte2 = mergeSort(array.slice(meio, array.length));
   array = ordena(parte1, parte2);
   }
+  
   return array;
+  
 }
   
 function ordena(parte1, parte2){
-  let posicaoAtual1 = 0;
-  let posicaoAtual2 = 0;
+  let posicaoAtualParte1 = 0;
+  let posicaoAtualParte2 = 0;
   const resultado = [];
-  while(posicaoAtual1 < parte1.length && posicaoAtual2 < parte2.length){
-    const produtoAtual1 = parte1[posicaoAtual1];
-    const produtoAtual2 = parte2[posicaoAtual2];
+  while(posicaoAtualParte1 < parte1.length && posicaoAtualParte2 < parte2.length){
+    const produtoParte1 = parte1[posicaoAtualParte1];
+    const produtoParte2 = parte2[posicaoAtualParte2];
     
-    if(produtoAtual1.preco < produtoAtual2.preco){
-      resultado.push(produtoAtual1);
-      posicaoAtual1++;
+    if(produtoParte1.preco < produtoParte2.preco){
+      resultado.push(produtoParte1);
+      posicaoAtualParte1++;
     }else{
-      resultado.push(produtoAtual2);
-      posicaoAtual2++
+      resultado.push(produtoParte2);
+      posicaoAtualParte2++
     }
+    
   }
-  return resultado.concat(posicaoAtual1 < parte1.length
-                         ? parte1.slice(posicaoAtual1)
-                         : parte2.slice(posicaoAtual2))
+  
+  return resultado.concat(posicaoAtualParte1 < parte1.length
+                         ? parte1.slice(posicaoAtualParte1)
+                         : parte2.slice(posicaoAtualParte2))
 }
 
-console.log(mergeSort(listaLivros))
+console.log(mergeSort(listaLivros));
 
 
 
